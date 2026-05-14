58ea93550d94e219d13809f7701a12e41db5a49b
Shreeja <kandacharam.shreeja@Validlyltd.com>
2026-05-14 21:54:46 +0530

    fix part two

--- Blogapp_frontend/src/components/AuthorDashboard.jsx
+++ Blogapp_frontend/src/components/AuthorDashboard.jsx
@@ -1,16 +1,1 @@
-import { Link, Outlet } from 'react-router-dom';
-
-function AuthorDashboard() {
-  return (
-    <div className="flex">
-      <nav className="w-1/4 bg-gray-100 h-screen p-5">
-        <Link to="add-article" className="block p-2 bg-blue-500 text-white rounded">Add New Article</Link>
-      </nav>
-      <main className="w-3/4 p-5">
-        <Outlet /> {/* This is where AddArticle will render */}
-      </main>
-    </div>
-  );
-}
-
-export default AuthorDashboard;
+a
--- Blogapp_frontend/src/utils/axiosInstance.js
+++ Blogapp_frontend/src/utils/axiosInstance.js
@@ -3,7 +3,8 @@ import axios from "axios";
 // Single axios instance for the entire app
 // Change baseURL here once when deploying to production
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true
 })
 
 export default axiosInstance;
