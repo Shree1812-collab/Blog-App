93140a0f5e781456ebea3562dcf740c2829102bd
Shree1812-collab <23eg110a20@anurag.edu.in>
2026-05-14 23:30:17 +0530

    added token to login response

--- Blogapp_backend/APIs/commonAPI.js
+++ Blogapp_backend/APIs/commonAPI.js
@@ -10,7 +10,11 @@ export const commonRouter = exp.Router();
 commonRouter.post("/login", async (req, res, next) => {
   try {
     const { token, user } = await login(req.body);
-    res.status(200).json({ message: "login success", payload: user });
+    res.status(200).json({
+  message: "login success",
+  token,
+  payload: user
+});
   } catch (err) {
     next(err);
   }
